c7e5a9f31e3104596d523e9d0afef1652dae6d47
Lucas Leal <lsleal@inf.ufrgs.br>
2018-11-07 17:27:51 +0100

    Server now works with img and html, send 404

--- assign-003/task-002/server.java
+++ assign-003/task-002/server.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.charset.Charset;
 import java.net.*;
 
 public class server {
@@ -18,28 +19,41 @@ public class server {
     }
 
     // Receiving client request
-    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-    String inputLine, resLine=null;
-    inputLine = in.readLine();
-    String[] tok = inputLine.split(" ");
-    if (!tok[0].equals("GET")) {
-      System.err.println(tok[0] + " not Implemented!");
+    // We open the client input as character stream and the output as bytes stream
+    BufferedReader clientIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    OutputStream clientOut = clientSocket.getOutputStream();
+
+    // Retrieve the first line of the packet "GET <filename> HTTP/x.x"
+    String[] reqLine = clientIn.readLine().split(" ");
+
+    // Treat request that are not GET
+    if(!reqLine[0].equals("GET")) {
+      System.err.println(reqLine[0]+" not implemented.");
       System.exit(1);
     }
 
-    // Sending HTTP response
-    resLine = "HTTP/1.1 200 OK\r\n\r\n";
-    BufferedReader fileReader = new BufferedReader(new FileReader(tok[1].split("/")[1]));
-    String fileInput;
-    while ((fileInput = fileReader.readLine()) != null) {
-      resLine += fileInput;
-      resLine += "\r\n";
-    }
-    out.println(resLine);
-    System.out.println("HTTP response:\n\n"+resLine);
+    // Try to open the file
+    try {
+      // If succeds read the file as a byte stream a write it to a byte array
+      FileInputStream fileIn = new FileInputStream(reqLine[1].split("/")[1]);
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      byte buffer[] = new byte[1024];
+      for(int s; (s=fileIn.read(buffer)) != -1;) {
+        baos.write(buffer, 0, s);
+      }
+      byte[] result = baos.toByteArray();
 
-    out.close(); in.close();
+      // Set the header response with appropriate encoding plus the byte array from the file
+      clientOut.write(new String("HTTP/1.1 200 OK\r\n\r\n").getBytes(Charset.forName("ISO-8859-1")));
+      clientOut.write(result);
+      clientOut.flush();
+      fileIn.close();
+    } catch (FileNotFoundException e) {
+      // If cannot open the file send a 404 error
+      clientOut.write(new String("HTTP/1.1 404 Not Found\r\n\r\n <!DOCTYPE html>\r\n<html>\r\n<head>\r\n<title>404</title>\r\n</head>\r\n<body>\r\n<h1>404 - Object not found</h1>\r\r</body>\r\n</html>").getBytes(Charset.forName("ISO-8859-1")));
+      clientOut.flush();
+    }
+
     clientSocket.close(); serverSocket.close();
   }
 }
